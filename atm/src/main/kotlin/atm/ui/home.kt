7b8aded0dc0398c276de3bdf0e5bef38036422a8
marioquintalcob <mario.e.quintal.cob@hotmail.com>
2023-03-31 16:20:04 -0600

    Cambios Mario Quintal en Login

--- atm/src/main/kotlin/atm/ui/home.kt
+++ atm/src/main/kotlin/atm/ui/home.kt
@@ -1,6 +1,9 @@
+import atm.entities.User
 import atm.usecases.CreateUser
+import atm.usecases.LoginUser
 
 class Home {
+    private val users = mutableListOf<User>()
     fun initHome() {
         println("BANCO NACIONAL BEDU S.A. de C.V." +
                 "Bienvenido a tu cajero. Selecciona una opción:\n" +
@@ -34,10 +37,44 @@ class Home {
         }
 
         val newUser = CreateUser(name, userName, password)
-        newUser.saveUser()
+        val id = generateId()
+        val hashPassword = generateHashPassword(password)
+        users.add(User(id, name, userName, 0.0, hashPassword))
+        println("Registro correcto, ya puedes iniciar sesión.\n" +
+                "Por favor ingresa tus datos:\n")
+        doLogin()
+    }
+
+    private fun generateHashPassword(password: String): String {
+        return password.hashCode().toString()
+
+    }
+
+    private fun generateId(): Int {
+        return -1
     }
 
     fun doLogin() {
+        print("Usuario: ")
+        var userName = readlnOrNull()
+        while (userName == null) {
+            print("Usuario: ")
+            userName = readlnOrNull()
+        }
+        print("Contraseña: ")
+        var password = readlnOrNull()
+        while (password == null) {
+            print("Contraseña: ")
+            password = readlnOrNull()
+        }
 
+        val loginUser = LoginUser(userName, password)
+        val user = loginUser.execute(users)
+        if (user != null) {
+            println("Bienvenido, ${user.name}!")
+            // TODO: Implement banking options for logged in user
+        } else {
+            println("Usuario o contraseña incorrectos.")
+        }
     }
 }
